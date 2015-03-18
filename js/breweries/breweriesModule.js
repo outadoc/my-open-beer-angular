23e3a90c490e9da7004be9f1025cdb80ba96d3d8
outadoc <outadoc@gmail.com>
2015-03-18 12:41:18 +0100

    Correction des erreurs sur les listes

--- js/breweries/breweriesModule.js
+++ js/breweries/breweriesModule.js
@@ -1,5 +1,5 @@
 var appBreweries = angular.module("BreweriesApp", []).
-	controller("BreweriesController", ["$scope", "rest", "$timeout", "$location", "config", "$route", "save", require("./breweriesController")]).
+	controller("BreweriesController", ["$scope", "rest", "$location", "config", "$route", "save", require("./breweriesController")]).
 	controller("BreweryAddController", ["$scope", "config", "$location", "rest", "save", "$document", "modalService", require("./breweryAddController")]).
 	controller("BreweryUpdateController", ["$scope", "config", "$location", "rest", "save", "$document", "modalService", "$controller", require("./breweryUpdateController")]);
 
