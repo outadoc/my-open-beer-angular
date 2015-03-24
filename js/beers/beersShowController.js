86b1e48a96ae679abd9556af51dc15a53ba8c630
Arckorus <lefevre.pierre.m.d@gmail.com>
2015-03-24 13:56:54 +0100

    la manière d'accéder au brasseur d'une bière se fait désormais de la bonne manière

--- js/beers/beersShowController.js
+++ js/beers/beersShowController.js
@@ -2,6 +2,7 @@ module.exports = function ($scope, rest, $timeout, $location, config, $route, sa
 
 	$scope.beer = config.activeBeer;
 	$scope.data = {load: false};
+	$scope.brewery = null;
 
 	if (angular.isUndefined(config.activeBeer)) {
 		$location.path("beers/");
@@ -9,21 +10,17 @@ module.exports = function ($scope, rest, $timeout, $location, config, $route, sa
 
 	if (config.breweries.mode === "online" || !config.breweries.loaded) {
 		$scope.data.load = true;
-		rest.getAll($scope.data, "breweries");
+		rest.getAll($scope.data, "breweries/" + $scope.beer.idBrewery, "breweries");
 		config.breweries.loaded = true;
 	} else {
 		$scope.data["breweries"] = config.breweries.all;
 	}
 
+	$scope.brewery = config.breweries;
+
 	$scope.show = function () {
-		brewery = null;
-		for (i = 0; i < $scope.data.breweries.length; i++) {
-			if ($scope.data.breweries[i].id == $scope.beer.idBrewery) {
-				brewery = $scope.data.breweries[i];
-			}
-		}
-		config.activeBrewery = angular.copy(brewery);
-		config.activeBrewery.reference = brewery;
+		config.activeBrewery = angular.copy($scope.brewery['all']);
+		config.activeBrewery.reference = $scope.brewery['all'];
 		$location.path("breweries/show");
 	};
 };
